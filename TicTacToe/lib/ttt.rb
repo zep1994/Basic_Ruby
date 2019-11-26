491f7238e5009fe9152cc452c4bad2c3d5700852
zep1994 <snaxbags@protonmail.com>
2019-11-26 17:40:47 -0500

    create tictact board and state

---
+++ TicTacToe/lib/ttt.rb
@@ -0,0 +1,9 @@
+#define board that will hold state
+board = Array.new(9, " ")
+
+#using asic to print board
+puts "   |   |   "
+puts "-----------"   
+puts "   |   |   "
+puts "-----------"   
+puts "   |   |   "
