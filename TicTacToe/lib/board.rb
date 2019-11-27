52844fbd90c2fbf1148d55d33bfd087330b4d117
zep1994 <snaxbags@protonmail.com>
2019-11-27 16:18:30 -0500

    Update board.rb

--- TicTacToe/lib/board.rb
+++ TicTacToe/lib/board.rb
@@ -2,8 +2,12 @@
 board = Array.new(9, " ")
 
 #using asic to print board
-puts "   |   |   "
-puts "-----------"   
-puts "   |   |   "
-puts "-----------"   
-puts "   |   |   "
+# puts "   |   |   "
+# puts "-----------"   
+# puts "   |   |   "
+# puts "-----------"   
+# puts "   |   |   "
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+  end
