103cb8c3b9614688828d3053b4c399d2596241ac
zep1994 <snaxbags@protonmail.com>
2019-11-27 16:15:52 -0500

    More Defining Methods

--- Methods/defining_methods.rb
+++ Methods/defining_methods.rb
@@ -1,3 +1,25 @@
 def greeting # Method Signature
     puts "Hello World" # Method Body
   end # Method Closing
+
+  num_of_attendees = 8
+#String Interpolation
+puts "There are #{num_of_attendees} people coming to Beyonce's birthday party."
+
+greeting
+
+bob_name           = "Bob"
+bob_age            = 46
+bob_occupation     = "Juggler"
+stefani_name       = "Stefani"
+stefani_age        = 49
+stefani_occupation = "Firefighter"
+ 
+puts "Name: #{bob_name}, Age: #{bob_age}, Occupation: #{bob_occupation}"
+ 
+# => "Name: Bob, Age: 46, Occupation: Juggler"
+ 
+puts "Name: #{stefani_name}, Age: #{stefani_age}, Occupation:
+#{stefani_occupation}"
+ 
+# => "Name: Stefani, Age: 49, Occupation: Firefighter"
