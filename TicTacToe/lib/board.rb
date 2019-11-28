d46955116588e2396eaf962a673e112d75cad0ab
zep1994 <snaxbags@protonmail.com>
2019-11-28 11:04:58 -0500

    Make the first move ruby

--- TicTacToe/lib/board.rb
+++ TicTacToe/lib/board.rb
@@ -12,6 +12,13 @@ def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input) 
-    input.to_i
+def input_to_index(user_input) 
+    "#{user_input}".to_i - 1
+end 
+
+#Move array should take three elements the array, the board, and the player's character
+#array[index] = value
+
+def move(board, index, player) 
+    board[index] = player 
 end 
