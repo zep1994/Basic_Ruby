5c2467e1af25e253b6fa6d916c7e960872dc05f2
zep1994 <snaxbags@protonmail.com>
2019-11-28 10:43:46 -0500

    turn input in to an integer

--- TicTacToe/lib/board.rb
+++ TicTacToe/lib/board.rb
@@ -10,4 +10,8 @@ board = Array.new(9, " ")
 
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
-  end
+end
+
+def input_to_index(input) 
+    input.to_i
+end 
